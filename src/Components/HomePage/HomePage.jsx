c7131f6a7b637a4f6b7ec6d46dee2da50521e787
Hager Atia <hager.atia2@gmail.com>
2024-09-11 02:41:14 +0300

    update home

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -15,10 +15,10 @@ const HomePage = () => {
         <div className="layout">
           <NavBar />
           <div className="text-center m-auto text-white pt-5">
-            <h1 className="fw-bold mb-5">مكتب المحاماة لزينب إبراهيم</h1>
+            <h1 className="fw-bold mb-5">مكتب المحامية زينب إبراهيم</h1>
             <h2 className="fw-bold lh-lg">
               {" "}
-              التمييـــــز في الدفـــاع
+              التمَيـــز في الدفـــاع
               <br /> والريادة في الاستشارات القانونية{" "}
             </h2>
           </div>
